39fb36fff4392e351f734223a0d6f61ec14f8401
hoseini <hoseini.1988@gmail.com>
2023-12-27 14:58:06 -0800

    SimpleFactory

---
+++ _01_SimpleFactory/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _01_SimpleFactory
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var door = DoorFactory.MakeDoor(80, 30);
+			Console.WriteLine($"Height of Door : {door.GetHeight()}");
+			Console.WriteLine($"Width of Door : {door.GetWidth()}");
+
+		}
+	}
+}
---
+++ _01_SimpleFactory/WoodenDoor.cs
@@ -0,0 +1,23 @@
+﻿namespace _01_SimpleFactory;
+
+public class WoodenDoor : IDoor
+{
+	private int Height { get; set; }
+	private int Width { get; set; }
+
+	public WoodenDoor(int height, int width)
+    {
+        this.Height = height;
+		this.Width = width;
+	}
+
+    public int GetHeight()
+	{
+		return this.Height;
+	}
+
+	public int GetWidth()
+	{
+		return this.Width;
+	}
+}
