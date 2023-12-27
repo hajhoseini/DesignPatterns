6bce1eee23747fe1877c0be8bc7eeb6627e025d7
hoseini <hoseini.1988@gmail.com>
2023-12-27 15:03:14 -0800

    Builder

---
+++ _04_Builder/Burger.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using System.Text;
+
+namespace _04_Builder;
+
+class Burger
+{
+	private int mSize;
+	private bool mCheese;
+	private bool mPepperoni;
+	private bool mLettuce;
+	private bool mTomato;
+
+    /*public Burger(int size, bool cheese, bool pepperoni, bool lettuce, bool tomato)
+	{
+		mSize = size;
+		mCheese = cheese;
+		mPepperoni = pepperoni;
+		mLettuce = lettuce;
+		mTomato = tomato;
+    }*/
+
+    public Burger(BurgerBuilder burgerBuilder)
+    {
+		mSize = burgerBuilder.Size;
+		mCheese = burgerBuilder.Cheese;
+		mPepperoni = burgerBuilder.Pepperoni;
+		mLettuce = burgerBuilder.Lettuce;
+		mTomato = burgerBuilder.Tomato;
+	}
+
+    public string GetDescription()
+	{
+		var sb = new StringBuilder();
+		sb.Append($"This is {this.mSize} inch Burger. ");
+		return sb.ToString();
+	}
+}
