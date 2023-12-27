ca8b51b592b4c63913e26afbb8729b3595f06434
hoseini <hoseini.1988@gmail.com>
2023-12-27 15:07:02 -0800

    Prototype

---
+++ _05_Prototype/Employee.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+
+namespace _05_Prototype;
+
+public class Employee : Person
+{
+	public Manager Manager { get; set; }
+	public override string Name { get; set; }
+
+    public Employee(string name, Manager manager)
+    {
+		Name = name;
+		Manager = manager;
+    }
+
+    public override Person Clone(bool deepClone = false)
+	{
+		if (deepClone)
+		{
+			var objectAsJson = JsonConvert.SerializeObject(this);
+			return JsonConvert.DeserializeObject<Employee>(objectAsJson);
+		}
+
+		return (Person)MemberwiseClone();
+	}
+}
