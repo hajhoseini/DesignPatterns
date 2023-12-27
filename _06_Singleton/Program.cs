18cd7d2e872b47982ca02037a2af94eacffa04c8
hoseini <hoseini.1988@gmail.com>
2023-12-27 15:08:57 -0800

    Singleton

---
+++ _06_Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _06_Singleton
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			President a = President.GetInstance();
+			President b = President.GetInstance();
+
+			Console.WriteLine(a == b); //Output : true
+		}
+	}
+}
