0009a3db1aa882c82eb55867c0add0ce32b6ae59
hoseini <hoseini.1988@gmail.com>
2023-12-27 21:16:42 -0800

    Facade

---
+++ _11_Facade/ComputerFacade.cs
@@ -0,0 +1,26 @@
+﻿namespace _11_Facade;
+
+class ComputerFacade
+{
+	private readonly Computer _computer;
+
+	public ComputerFacade(Computer computer)
+    {
+		_computer = computer;
+	}
+
+	public void TurnOn()
+	{
+		_computer.GetElectricShock();
+		_computer.MakeSound();
+		_computer.ShowLoadingScreen();
+		_computer.Bam();
+	}
+
+	public void TurnOff()
+	{
+		_computer.CloseEverything();
+		_computer.PullCurrent();
+		_computer.Sooth();
+	}
+}
---
+++ _11_Facade/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _11_Facade
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var computer = new ComputerFacade(new Computer());
+
+			computer.TurnOn(); // Ouch! Beep beep! Loading.. Ready to be used!
+			Console.WriteLine();
+
+			computer.TurnOff();  // Bup bup buzzz! Haah! Zzzzz
+			Console.ReadLine();
+		}
+	}
+}
