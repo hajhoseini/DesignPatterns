deedbc919a7f20a1ebd302c44ce7d0bd159e20ae
hoseini <hoseini.1988@gmail.com>
2023-12-27 18:11:52 -0800

    Adapter

---
+++ _07_Adapter/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _07_Adapter
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var wildDog = new WildDog();
+			var wildDogAdapter = new WildDogAdapter(wildDog);
+
+			var hunter = new Hunter();
+			hunter.Hunt(wildDogAdapter);
+		}
+	}
+}
---
+++ _07_Adapter/WildDogAdapter.cs
@@ -0,0 +1,18 @@
+﻿using _07_Adapter.Lion;
+
+namespace _07_Adapter;
+
+class WildDogAdapter : ILion
+{
+    private WildDog _dog;
+
+    public WildDogAdapter(WildDog dog)
+    {
+		_dog = dog;
+	}
+
+    public void Roar()
+	{
+		_dog.bark();
+	}
+}
