3edaa434eb304935b58f89f70870a60d0c51f05a
hoseini <hoseini.1988@gmail.com>
2023-12-29 00:40:52 -0800

    Iterator

---
+++ _16_Iterator/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _16_Iterator
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var stations = new StationList();
+
+			var station1 = new RadioStation(89);
+			stations.Add(station1);
+
+			var station2 = new RadioStation(101);
+			stations.Add(station2);
+
+			var station3 = new RadioStation(102);
+			stations.Add(station3);
+
+			foreach (var x in stations)
+			{
+				Console.Write(x.GetFrequecy());
+			}
+
+			var q = stations.Where(x => x.GetFrequecy() == 89).FirstOrDefault();
+			Console.WriteLine(q.GetFrequecy());
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ _16_Iterator/RadioStation.cs
@@ -0,0 +1,16 @@
+﻿namespace _16_Iterator;
+
+class RadioStation
+{
+	private float mFrequency;
+
+	public RadioStation(float frequency)
+	{
+		mFrequency = frequency;
+	}
+
+	public float GetFrequecy()
+	{
+		return mFrequency;
+	}
+}
