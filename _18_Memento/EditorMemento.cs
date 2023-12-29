c83f279ebf2a75539bf15bd295c624e8eb7864cd
hoseini <hoseini.1988@gmail.com>
2023-12-29 01:03:45 -0800

    Memento

---
+++ _18_Memento/EditorMemento.cs
@@ -0,0 +1,17 @@
+﻿namespace _18_Memento
+{
+	class EditorMemento
+	{
+		private string _content;
+
+        public EditorMemento(string content)
+        {
+			_content = content;
+        }
+
+		public string Content
+		{
+			get { return _content; }
+		}
+    }
+}
---
+++ _18_Memento/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _18_Memento
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var editor = new Editor();
+
+			//Type some stuff
+			editor.Type("This is the first sentence.");
+			editor.Type("This is second.");
+
+			// Save the state to restore to : This is the first sentence. This is second.
+			editor.Save();
+
+			//Type some more
+			editor.Type("This is third.");
+
+			//Output the content
+			Console.WriteLine(editor.Content); // This is the first sentence. This is second. This is third.
+
+			//Restoring to last saved state
+			editor.Restore();
+
+			Console.Write(editor.Content); // This is the first sentence. This is second
+		}
+	}
+}
